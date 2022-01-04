4ecd03e2853932d5f9ade0e35d70b64e23df9721
Iulian Oana <iulian.andreioana@gmail.com>
2022-01-04 08:12:52 +0000

    Added CoffeeShopService

---
+++ API/Models/CoffeeShopModel.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+	public class CoffeeShopModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string OpeningHours { get; set; }
+		public string Address { get; set; }
+	}
+}
---
+++ API/Services/CoffeeShopService.cs
@@ -0,0 +1,30 @@
+﻿using API.Models;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+	public class CoffeeShopService : ICoffeeShopService
+	{
+        private readonly ApplicationDbContext dbContext;
+
+        public CoffeeShopService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<List<CoffeeShopModel>> List()
+        {
+            var coffeeShops = await (from shop in dbContext.CoffeeShops
+                                     select new CoffeeShopModel()
+                                     {
+                                         Id = shop.Id,
+                                         Name = shop.Name,
+                                         OpeningHours = shop.OpeningHours,
+                                         Address = shop.Address
+                                     }).ToListAsync();
+
+            return coffeeShops;
+        }
+    }
+}
