81b241fac18b960de5eecebda2e8e7782a824809
Iulian Oana <iulian.andreioana@gmail.com>
2022-01-12 07:18:16 +0000

    Set up OIDC in our Client application

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
 using Client.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -11,6 +13,28 @@ builder.Services.AddHttpClient();
 
 builder.Services.Configure<IdentityServerSettings>(builder.Configuration.GetSection("IdentityServerSettings"));
 builder.Services.AddScoped<ITokenService, TokenService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(
+    OpenIdConnectDefaults.AuthenticationScheme,
+    options =>
+    {
+        options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.SignOutScheme = OpenIdConnectDefaults.AuthenticationScheme;
+        options.Authority = builder.Configuration["InteractiveServiceSettings:AuthorityUrl"];
+        options.ClientId = builder.Configuration["InteractiveServiceSettings:ClientId"];
+        options.ClientSecret = builder.Configuration["InteractiveServiceSettings:ClientSecret"];
+
+        options.ResponseType = "code";
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+    }
+);
 
 var app = builder.Build();
 
--- Server/Program.cs
+++ Server/Program.cs
@@ -40,14 +40,16 @@ builder.Services.AddIdentityServer()
     })
     .AddDeveloperSigningCredential();
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseIdentityServer();
-//app.UseAuthorization();
-//app.UseEndpoints(endpoints =>
-//{
-//	endpoints.MapDefaultControllerRoute();
-//});
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
